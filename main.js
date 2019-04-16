9ae12ccd8185054445f3c43e947bda6750eeed82
Pelaez <0409184@students.dadeschools.net>
2019-04-16 12:01:33 -0400

    did

---
+++ main.js
@@ -0,0 +1,22 @@
+var panel = $("#quiz-area");
+
+var countStartNumber = 30;
+
+// Question Set
+var questions = [
+
+    {
+        question: "Which was the first Pokemon ever designed?",
+        answers: ["Pikachu", "Rhydon", "Onix", "Bulbasaur"],
+        correctAnswer: "Rhydon",
+        image: "assets\Rhydon.gif"
+    },
+
+    {
+        question: "Which of these Pokemon actually exists?",
+        answers: ["Garbodor", "Hissune", "Stronk", "Surinari"],
+        correctAnswer: "Garbodor",
+        image: "assets\Trubbish.gif"
+    },
+
+]
